898e1e4ad521608ad77352556290a0d8806603f6
Preeti Desale <desalepreeti@gmail.com>
2026-07-02 22:30:36 +0530

    adding frontend on git

--- backend/server.js
+++ backend/server.js
@@ -1,92 +1,88 @@
+
 require("dotenv").config();
 
-const express =
-require("express");
+const express = require("express");
 
-const cors =
-require("cors");
+const cors = require("cors");
 
-const connectDB =
-require("./config/db");
+const connectDB = require("./config/db");
 
-const authRoutes =
-require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 
-const productRoutes =
-require("./routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-const cartRoutes =
-require("./routes/cartRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
-const wishlistRoutes =
-require("./routes/wishlistRoutes");
+const wishlistRoutes = require("./routes/wishlistRoutes");
 
-const orderRoutes =
-require("./routes/orderRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
-const reviewRoutes =
-require("./routes/reviewRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 
-// DB
 
+// DATABASE
 connectDB();
 
-// MIDDLEWARE
 
+// MIDDLEWARE
 app.use(cors());
 
 app.use(express.json());
 
-// TEST ROUTE
 
+// TEST ROUTE
 app.get("/", (req, res) => {
 
-  res.send(
-    "ShopSphere API Running 🚀"
-  );
+res.send(
+"ShopSphere API Running 🚀"
+);
 
 });
 
-// AUTH ROUTES
 
+// ROUTES
 app.use(
-  "/api/auth",
-  authRoutes
-);
-
-app.use(
-  "/api/products",
-  productRoutes
+"/api/auth",
+authRoutes
 );
 
 app.use(
-  "/api/cart",
-  cartRoutes
+"/api/products",
+productRoutes
 );
 
 app.use(
-  "/api/wishlist",
-  wishlistRoutes
+"/api/cart",
+cartRoutes
 );
+
 app.use(
-  "/api/orders",
-  orderRoutes
+"/api/wishlist",
+wishlistRoutes
 );
+
 app.use(
-  "/api/reviews",
-  reviewRoutes
+"/api/orders",
+orderRoutes
 );
+
+app.use(
+"/api/reviews",
+reviewRoutes
+);
+
+
 // PORT
-
 const PORT =
 process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
-  console.log(
-    `Server Running On Port ${PORT}`
-  );
+console.log(
+`Server Running On Port ${PORT}`
+);
 
 });
+
